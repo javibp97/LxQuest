4474816447f17ef8f2d3e45ca6abe142f48f9985
javi <javibp97@gmail.com>
2026-04-19 18:35:32 +0200

    Actualizar índices Distribucion y usuario dentro de Favorito.kt y Nota.kt

--- app/src/main/java/com/jbonet/lxquest/data/entities/Favorito.kt
+++ app/src/main/java/com/jbonet/lxquest/data/entities/Favorito.kt
@@ -3,6 +3,7 @@ package com.jbonet.lxquest.data.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.ForeignKey
+import androidx.room.Index
 
 // @Suppress("PropertyName") --> Se ignora camelCase para mantener consistencia con snake_case.
 /** La siguiente entidad gestiona las distribuciones marcadas como favoritas. */
@@ -17,7 +18,12 @@ import androidx.room.ForeignKey
             entity = Usuario::class,
             parentColumns = ["id_usuario"],
             childColumns = ["id_usuario"],
-            onDelete = ForeignKey.NO_ACTION)])
+            onDelete = ForeignKey.NO_ACTION)],
+    // Creación de índice para distribuciones o usuario.
+    indices = [
+        Index(value = ["id_distro"]),
+        Index(value = ["id_usuario"])]
+)
 data class Favorito(
     @PrimaryKey(autoGenerate = true)
     val id_favorito: Int = 0,
--- app/src/main/java/com/jbonet/lxquest/data/entities/Nota.kt
+++ app/src/main/java/com/jbonet/lxquest/data/entities/Nota.kt
@@ -1,12 +1,29 @@
 package com.jbonet.lxquest.data.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 /** Entidad que introduce las notas del usuario sobre una distribución en concreto. */
 // @Suppress("PropertyName") --> Se ignora camelCase para mantener consistencia con snake_case.
 @Suppress("PropertyName")
-@Entity
+@Entity(
+tableName = "Nota",
+foreignKeys = [ForeignKey(
+        entity = Distribucion::class,
+        parentColumns = ["id_distro"],
+        childColumns = ["id_distro"],
+        onDelete = ForeignKey.CASCADE),
+ForeignKey(
+entity = Usuario::class,
+parentColumns = ["id_usuario"],
+childColumns = ["id_usuario"],
+onDelete = ForeignKey.CASCADE)],
+indices = [
+    Index(value = ["id_distro"]),
+    Index(value = ["id_usuario"])]
+)
 data class Nota(@PrimaryKey(autoGenerate = true)
     val id_nota: Int = 0,
     val id_distro: Int,
